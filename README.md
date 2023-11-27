6e3d80f41e40b63e5cbd461ea9e9097c481e3c04
Terence Ponce <terenceponce@outlook.com>
2023-11-27 23:39:23 +0000

    Add Github Actions workflow

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Leet Code Practice
 
+[![gh-actions](https://github.com/terenceponce/leetcode/workflows/CI/badge.svg)](https://github.com/terenceponce/leetcode/actions?workflow=CI)
+
 These are my solutions to LeetCode problems written in Elixir.
--- mix.exs
+++ mix.exs
@@ -5,9 +5,10 @@ defmodule LeetCodePractice.MixProject do
     [
       app: :leetcode,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "1.15.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -23,6 +24,13 @@ defmodule LeetCodePractice.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: ["format"],
+      "lint.ci": ["format --check-formatted"]
     ]
   end
 end
