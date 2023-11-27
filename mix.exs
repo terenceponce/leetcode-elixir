4c6cfb6670a47d3c0ae5e7e7c4e518caa2621e68
Terence Ponce <terenceponce@outlook.com>
2023-11-27 23:50:21 +0000

    Add Github Actions workflow (#1)
    This integrates GitHub Actions as well as Credo to run CI checks on this repo.

--- mix.exs
+++ mix.exs
@@ -5,9 +5,10 @@ defmodule LeetCodePractice.MixProject do
     [
       app: :leetcode,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "1.15.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -21,8 +22,14 @@ defmodule LeetCodePractice.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "1.7.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: ["format", "credo"],
+      "lint.ci": ["format --check-formatted", "credo"]
     ]
   end
 end
